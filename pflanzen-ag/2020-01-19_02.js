6f0a0c9d76232474c14ccc6edce1ba36c1e4a971
nabubremerhaven <61091241+nabubremerhaven@users.noreply.github.com>
2020-02-15 18:28:57 +0100

    Create 2020-01-19_02.js

---
+++ pflanzen-ag/2020-01-19_02.js
@@ -0,0 +1,30 @@
+ev0201 = ["ev0201", "AG-Treffen", "Mittwoch, 26.02.",
+         "Thema: Salzwiesen",
+         "Pflanzen-AG", "", "18:30", "", "Deichvorland (Foto: Witt)", "https://i.imgur.com/G6YVdg5.jpg"]
+
+ev0301 = ["ev0301", "AG-Treffen", "Mittwoch, 25.03.",
+         "Thema: Salzwiesen",
+         "Pflanzen-AG", "", "18:30", "", "Deichvorland (Foto: Witt)", "https://i.imgur.com/G6YVdg5.jpg"]
+
+ev0401 = ["ev0401", "AG-Treffen: Frühblüher im Bürgerpark", "Mittwoch, 29.04.",
+         "Themen: Frühblüher im Bürgerpark, Planung der Quartalsaktivitäten.",
+         "Pflanzen-AG", "", "18:30", "Bürgerpark, Parkeingang Bismarckstraße, Bremerhaven", "Krokusse (Foto: Witt)", "https://i.imgur.com/TyAKRV2.jpg"]
+
+ev0501 = ["ev0501", "AG-Treffen: Imsumer Salzwiesen & Ochsenturm", "Mittwoch, 13.05.",
+         "Thema: Imsumer Salzwiesen & Ochsenturm",
+         "Pflanzen-AG", "", "18:30", "Ochsenturm, Beim Ochsentum, Geestland-Imsum", "Deichvorland (Foto: Witt)", "https://i.imgur.com/G6YVdg5.jpg"]
+
+ev0601 = ["ev0601", "AG-Treffen: Imsumer Salzwiesen & Ochsenturm", "Mittwoch, 24.06.",
+         "Thema: Imsumer Salzwiesen & Ochsenturm",
+         "Pflanzen-AG", "", "18:30", "Ochsenturm, Beim Ochsentum, Geestland-Imsum", "Deichvorland (Foto: Witt)", "https://i.imgur.com/G6YVdg5.jpg"]
+
+ev0602 = ["ev0602", "Führung: Imsumer Salzwiesen & Ochsenturm", "Sonntag, 28.06.",
+         "Thema: Imsumer Salzwiesen & Ochsenturm",
+         "Pflanzen-AG", "", "14:00", "Ochsenturm, Beim Ochsentum, Geestland-Imsum", "Deichvorland (Foto: Witt)", "https://i.imgur.com/G6YVdg5.jpg"]
+
+events = [ev0201, ev0301, ev0401, ev0501, ev0601, ev0602]
+
+for (let i of events) {
+    for (j = 0, len = i.length; j < len; j++) {
+        if (j < len - 1) {document.getElementById(i[0]).getElementsByTagName("p")[j].innerHTML=i[j]}
+        else {document.getElementById(i[0]).getElementsByTagName("img")[0].src=i[j]}}}
