54d7cf32c6acc81326995c9bf896d68d17bfd965
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-03-05 19:01:29 +0100

    Create 2020-03-05_aktiven_01.js

---
+++ vogel-ag/2020-03-05_aktiven_01.js
@@ -0,0 +1,10 @@
+ev0301 = ["ev0301", "AG-Treffen: Polder Bramel", "Donnerstag, 12.03.",
+         "Thema: Salzwiesen",
+         "Vogel-AG", "", "17:45", "Beobachtungsturm am Polder Bramel, Schiffdorf-Bramel", "", ""]
+
+events = [ev0301]
+
+for (let i of events) {
+    for (j = 0, len = i.length; j < len; j++) {
+        if (j < len - 1) {document.getElementById(i[0]).getElementsByTagName("p")[j].innerHTML=i[j]}
+        else {document.getElementById(i[0]).getElementsByTagName("img")[0].src=i[j]}}}
