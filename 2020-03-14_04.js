47c8f7301ad2aac9f18703b3d947a233c0bd369a
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-03-14 17:55:30 +0100

    Update 2020-03-14_04.js

--- 2020-03-14_04.js
+++ 2020-03-14_04.js
@@ -4,7 +4,7 @@ pflanzen0301 = ["pflanzen0301", "Pflanzen-AG: Salzwiesen", "Mittwoch, 25.03.",
          
 vogel0301 = ["vogel0301", "Vogel-AG: Polder Bramel", "Donnerstag, 12.03.",
          "Thema: Vogelbeobachtungen am Polder Bramel",
-         "", "", "17:45", "Beobachtungsturm am Polder Bramel, Schiffdorf-Bramel"]
+         "", "", "17:45", "<a href='https://www.google.com/maps/place/53%C2%B033'32.3%22N+8%C2%B042'56.6%22E/@53.5577007,8.7119651,15z/'>Beobachtungsturm am Polder Bramel, Schiffdorf-Bramel</a>"]
 
 events = [pflanzen0301, vogel0301]
 
